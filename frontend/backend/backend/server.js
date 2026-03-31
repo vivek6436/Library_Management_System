a11114adf89bc3b173922b94e0c259f059347728
VIVEK KUMAR <vivekrock1090@gmail.com>
2026-03-31 17:17:01 +0530

    Create server.js

---
+++ frontend/backend/backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
